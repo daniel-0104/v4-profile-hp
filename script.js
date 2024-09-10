20392280ec66c66ac5423aa5344cad0a9eed0c73
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-09-10 17:17:09 +0630

    10 noon last commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar-example'
-});
+// const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar-example'
+// });
 
 window.addEventListener('scroll',reveal);
 function reveal(){
@@ -19,10 +19,6 @@ function reveal(){
   }
 }
 
-// window.addEventListener('load', function() {
-//   document.getElementById('preloader').style.display = 'none';
-// });
-
 
 var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
@@ -39,3 +35,43 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".button-prev",
   }
 });
+
+
+function sendEmail(event) {
+  event.preventDefault();
+    if( document.getElementById('name').value != "" && document.getElementById('phone').value != "" && 
+        document.getElementById('email').value != "" && document.getElementById('message').value != "") 
+    {
+        let params = {
+            name: document.getElementById('name').value,
+            phone: document.getElementById('phone').value,
+            email: document.getElementById('email').value,
+            message:document.getElementById('message').value
+        };
+
+        var phonePattern = /^(09|\+959)\d{9}$/;
+        if (!phonePattern.test(params.phone)) {
+          alert("Please enter a valid phone number");
+          return;
+        }
+
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailPattern.test(params.email)) {
+            alert("Please enter a valid email address");
+            return;
+        }
+
+        emailjs.send("service_os3ctl7","template_dxcqnqf",params)
+        .then((res) => {
+            document.getElementById('name').value = "";
+            document.getElementById('phone').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('message').value = "";
+            alert("Your message has been sent successfully . Status code : " + res.status);
+        });
+    }   
+    else 
+    {
+      alert("please fill all information!!!");
+    }
+}
